9f7042327bd40e9f252617826446ddc0dcaa7b40
william <william@williams-MacBook-Pro.local>
2022-08-10 20:51:22 +0100

    data fixed3

--- components/BirthDayCard.jsx
+++ components/BirthDayCard.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 // import {weekdays} from ".././weekDaysData"
 
-import {weekdays}  from "../NumberData"
+import {weekdays}  from ".././NumberData"
 
 function BirthDayCard() {
   const userData = useSelector((state) => state.weekday.value)
